961e0dc2f32510fc58f3bd226d8d812422876ee0
joaoviesilva0811-star <joaoviesilva0811@gmail.com>
2026-07-18 14:05:17 -0300

    Delete includes directory

--- includes/partials/divisor.php
+++
@@ -1,3 +0,0 @@
-<svg class="divisor-glace" viewBox="0 0 1200 40" preserveAspectRatio="none" aria-hidden="true">
-  <path d="M0,0 C50,40 100,40 150,0 C200,40 250,40 300,0 C350,40 400,40 450,0 C500,40 550,40 600,0 C650,40 700,40 750,0 C800,40 850,40 900,0 C950,40 1000,40 1050,0 C1100,40 1150,40 1200,0 L1200,40 L0,40 Z"></path>
-</svg>
--- includes/partials/head.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Cardápio online de bolos da Delícias da Maria. Escolha o sabor, cobertura e recheio, e peça diretamente pelo WhatsApp.">
-<link rel="stylesheet" href="<?= $urlBase ?? '' ?>assets/css/style.css">
-<link rel="icon" type="image/png" sizes="32x32" href="<?= $urlBase ?? '' ?>assets/img/favicon-32.png">
-<link rel="icon" type="image/png" sizes="64x64" href="<?= $urlBase ?? '' ?>assets/img/favicon.png">
